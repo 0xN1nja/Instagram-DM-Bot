1d2f4c452e1852d9e3f16058413db74af310d6dc
Abhimanyu Sharma <71812498+N1nja0p@users.noreply.github.com>
2021-12-17 09:33:15 +0530

    .

---
+++ bot.py
@@ -0,0 +1,99 @@
+# Copyright Abhimanyu Sharma, https://github.com/N1nja0p
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support import expected_conditions as EC
+import time
+import os
+import discord_notifications
+import datetime
+# Constants
+CURRENT_TIME=lambda:datetime.datetime.now().strftime("%H:%M:%S")
+TEMP_TIME=lambda:datetime.datetime.now().strftime("%H:%M")
+MESSAGE='''''' # Add Message To Send
+TARGET_USERNAME="" # Add Target's Username
+SENDING_TIME="00:00" # 24hr Format // 00:00
+SHUTDOWN=False # Set This To True To Shutdown PC After Sending Message (Optional)
+CHROME_DRIVER_PATH=r"" # Download Chrome Driver (https://chromedriver.chromium.org/)
+CREDENTIALS={"username":"","password":""} # Add Your Credentials
+chrome_options=Options()
+chrome_options.add_argument("--start-maximized")
+class DMBOT():
+    def __init__(self,username:str,password:str):
+        self.driver=webdriver.Chrome(executable_path=CHROME_DRIVER_PATH,options=chrome_options)
+        self.driver.get("https://instagram.com")
+        self.username=username
+        self.password=password
+    def login(self):
+        try:
+            WebDriverWait(self.driver,100000).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="loginForm"]/div/div[1]/div/label/input')))
+            self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[1]/div/label/input').send_keys(self.username)
+            self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[2]/div/label/input').send_keys(self.password)
+            self.driver.find_element_by_xpath('//*[@id="loginForm"]/div/div[3]/button').click()
+            WebDriverWait(self.driver,10000000000).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="react-root"]/section/main/div/div/div/div/button')))
+            self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/div/div/div/button').click()
+            time.sleep(10) # Replace With 60 To Avoid Error
+            self.driver.execute_script('document.querySelector("body > div.RnEpo.Yx5HN > div > div > div > div.mt3GC > button.aOOlW.HoLwm").click()')
+        except:
+            discord_notifications.notify("login-failed",CURRENT_TIME())
+            print("Login Failed!")
+        else:
+            discord_notifications.notify("login-success",CURRENT_TIME())        
+            print("Login Success!")
+    def find_target(self):
+        try:
+            self.driver.get("https://www.instagram.com/direct/new/")
+            WebDriverWait(self.driver,10000).until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[2]/div/div/div[2]/div[1]/div/div[2]/input')))
+            target_username=self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div[1]/div/div[2]/input')
+            time.sleep(5)
+            target_username.send_keys(TARGET_USERNAME)
+            WebDriverWait(self.driver,100000).until(EC.visibility_of_element_located((By.XPATH,'/html/body/div[2]/div/div/div[2]/div[2]/div[1]/div')))
+            time.sleep(2)
+            self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[2]/div[2]/div[1]/div').click()
+            time.sleep(2)
+            self.driver.find_element_by_xpath('/html/body/div[2]/div/div/div[1]/div/div[2]/div/button/div').click()
+            time.sleep(5)
+        except:
+            discord_notifications.notify("target-not-found",CURRENT_TIME(),TARGET_USERNAME)
+        else:
+            discord_notifications.notify("found-target",CURRENT_TIME())
+            print(f"Target Found!")
+    def send_message(self):
+        try:
+            WebDriverWait(self.driver,100000).until(EC.visibility_of_element_located((By.XPATH,'//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea')))
+            message_textarea=self.driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[2]/div/div[2]/div/div/div[2]/textarea')
+            time.sleep(5)
+            message_textarea.send_keys(MESSAGE)
+            self.target_name=str(self.driver.find_element_by_xpath('//*[@id="react-root"]/section/div/div[2]/div/div/div[2]/div[1]/div/div/div[2]/div/div[2]/button/div/div/div').get_attribute("innerHTML"))
+        except:
+            self.message_sent=False
+        else:
+            discord_notifications.notify("message-sent",CURRENT_TIME(),self.target_name)
+            print(f"Birthday Wished! Target Name : {self.target_name}")
+            self.message_sent=True
+    def shutdown_pc(self):
+        discord_notifications.notify("shuttingdown-pc",CURRENT_TIME())
+        print("Shutting Down PC!")
+        time.sleep(10)
+        with open(r"message.log","a",encoding="utf-8")as f:
+            f.write(f"{CURRENT_TIME()} : Birthday Wished : {self.message_sent}, Target Name : {self.target_name}"+"\n")
+        # Don't Shutdown PC rn
+        if SHUTDOWN:
+            os.system("shutdown /s /t 1")
+if __name__ == "__main__":
+    dm_bot=DMBOT(CREDENTIALS["username","password"])
+    dm_bot.login()
+    dm_bot.find_target()
+    # Scheduled
+    while True:
+        if TEMP_TIME() == SENDING_TIME:
+            dm_bot.send_message()
+            break
+    ############################# NON SCHEDULED ##############################
+    # Non Scheduled
+    # dm_bot.send_message()
+    ##########################################################################
+    time.sleep(60)
+    dm_bot.shutdown_pc()
---
+++ discord_notifications.py
@@ -0,0 +1,26 @@
+from discord_webhooks import DiscordWebhooks
+def notify(status:str,time:str,target_name:str=None):
+    WEBHOOK_URL='' # Enter Your Webhook URL
+    webhook=DiscordWebhooks(WEBHOOK_URL)
+    webhook.set_footer(text="--Abhimanyu Sharma")
+    if status=="login-success":
+        webhook.set_content(title="Login Success!",description="Here's Your Report With :heart:")
+        webhook.add_field(name="Time",value=str(time))
+    if status=="login-failed":
+        webhook.set_content(title="Login Failed!",description="Here's Your Report With :heart:")
+        webhook.add_field(name="Time",value=str(time))
+    if status=="message-sent":
+        webhook.set_content(title="Message Sent!",description="Here's Your Report With :heart:")
+        webhook.add_field(name="Time",value=str(time))
+        webhook.add_field(name="Target's Name",value=str(target_name))
+    if status=="found-target":
+        webhook.set_content(title="Found Target",description="Here's Your Report With :heart:")
+        webhook.add_field(name="Time",value=str(time))
+    if status=="target-not-found":
+        webhook.set_content(title="Target Not Found",description="Here's Your Report With :heart:")
+        webhook.add_field(name="Time",value=str(time))
+        webhook.add_field(name="Provided Username",value=str(target_name))
+    if status=="shuttingdown-pc":
+        webhook.set_content(title="Shutting Down Your PC",description="Here's Your Report With :heart:")
+        webhook.add_field(name="Time",value=str(time))
+    webhook.send()
